23a8c966965a951d4a7c8b336d68c0acb88e4cba
IMAWarriors <sillysquadweekly@gmail.com>
2026-03-19 01:58:58 -0400

    VERSION E-Build Alpha 1.0.0 [Stablized Push!]: Stabilized Camera and Render interpolation

--- main.cpp
+++ main.cpp
@@ -1,5 +1,24 @@
 // main.cpp
 
+/**    =======================================================================================================
+ *      > MAIN FILE, Version E-Build Alpha 1.0.0
+ *      > by IMAWarriors
+ *      --------------------------------------
+ *      > Stable working version pushed to Git on 03-19-2029, 2:00 AM 
+ * 
+ *      >>> Most Recently Added Features
+ *       - Stabilized Camera and Render interpolation
+ *       - Added debug basic overlay
+ *       - Split engine and application dependencies
+ * 
+ *      >>> Working-in-Progress Features (as of this push, goals/objectives)
+ *       - Add queue for consistnent entity / component management WITHOUT view reading sequentially messing things up
+ *       - Tilemap...
+ * 
+ * 
+ *    ======================================================================================================= */
+
+
 // C++ Util Includes
 #include <algorithm>
 #include <cmath>
