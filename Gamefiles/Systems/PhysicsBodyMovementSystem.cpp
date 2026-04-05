8a538795b3196d663aebf6f39a3d2fdf05ac7983
IMAWarriors <sillysquadweekly@gmail.com>
2026-04-05 01:07:59 -0400

    X/Y Collision?

--- Gamefiles/Systems/PhysicsBodyMovementSystem.cpp
+++ Gamefiles/Systems/PhysicsBodyMovementSystem.cpp
@@ -2,6 +2,8 @@
 
 #include "PhysicsBodyMovementSystem.h"
 
+#include <vector>
+
 void PhysicsBodyMovementSystem::update (Registry & registry, float deltatime) {
 
 
@@ -23,32 +25,38 @@ void PhysicsBodyMovementSystem::update (Registry & registry, float deltatime) {
         int direction_x = (velocity.magnitude.x > 0.0f) ? 1 : (velocity.magnitude.x < 0.0f) ? -1 : 0;
         int direction_y = (velocity.magnitude.y > 0.0f) ? 1 : -1;
 
-        static const int HORZ_RAY_SPACING = 10;
-        static const int HORZ_RAY_COUNT = std::max(2, (int)(body.size.y / HORZ_RAY_SPACING));
+        const int HORZ_RAY_SPACING = 10;
+        const int HORZ_RAY_COUNT = std::max(2, (int)std::ceil(body.size.y / (float)HORZ_RAY_SPACING));
+        std::vector<float> horz_ray_dists(HORZ_RAY_COUNT, std::fabs(to_move.x));
 
-        float horz_ray_dists[HORZ_RAY_COUNT+1];
-
-        
+        const int VERT_RAY_SPACING = 10;
+        const int VERT_RAY_COUNT = std::max(2, (int)std::ceil(body.size.x / (float)VERT_RAY_SPACING));
+        std::vector<float> vert_ray_dists(VERT_RAY_COUNT, std::fabs(to_move.y));
 
 
         for (TileGrid & layer : scene.tile_layers) {
+
+            // HORIZONTAL COLLISION
+
+            // Cast horizontal rays
+
             if (direction_x > 0) {
-                for (int i = 0; i < HORZ_RAY_COUNT-1; i++) {
-                    horz_ray_dists[i] = layer.raycast(scene, {transform.position.x + body.size.x / 2, (transform.position.y - body.size.y / 2) + i * HORZ_RAY_SPACING}, {1.0, 0}, (int)std::ceil(std::fabs(to_move.x)));
+                for (int i = 0; i < HORZ_RAY_COUNT; i++) {
+                    float ray_y = (transform.position.y - body.size.y / 2.0f) + ((body.size.y * i) / (float)(HORZ_RAY_COUNT - 1));
+                    horz_ray_dists[i] = layer.raycast(scene, {transform.position.x + body.size.x / 2.0f, ray_y}, {1.0f, 0.0f}, (int)std::ceil(std::fabs(to_move.x)));
                 }
-                horz_ray_dists[HORZ_RAY_COUNT] = layer.raycast(scene, {transform.position.x + body.size.x / 2, transform.position.y + body.size.y / 2}, {1.0, 0}, (int)std::ceil(std::fabs(to_move.x)));
+                
             } else if (direction_x < 0) {
-                for (int i = 0; i < HORZ_RAY_COUNT-1; i++) {
-                    horz_ray_dists[i] = layer.raycast(scene, {transform.position.x - body.size.x / 2, (transform.position.y - body.size.y / 2) + i * HORZ_RAY_SPACING}, {-1.0, 0}, (int)std::ceil(std::fabs(to_move.x)));
+                for (int i = 0; i < HORZ_RAY_COUNT; i++) {
+                    float ray_y = (transform.position.y - body.size.y / 2.0f) + ((body.size.y * i) / (float)(HORZ_RAY_COUNT - 1));
+                    horz_ray_dists[i] = layer.raycast(scene, {transform.position.x - body.size.x / 2.0f, ray_y}, {-1.0f, 0.0f}, (int)std::ceil(std::fabs(to_move.x)));
                 }
-                horz_ray_dists[HORZ_RAY_COUNT] = layer.raycast(scene, {transform.position.x - body.size.x / 2, transform.position.y + body.size.y / 2}, {-1.0, 0}, (int)std::ceil(std::fabs(to_move.x)));
+
             } else {
-
-
                 break;
             }
 
-            bool in_wall = false;
+            // Set move to horizontal ray calc dist min clamp to collision
 
             for (float dist : horz_ray_dists) {
                 if (dist < std::fabs(to_move.x)) {
@@ -56,56 +64,7 @@ void PhysicsBodyMovementSystem::update (Registry & registry, float deltatime) {
 
                     if (dist == -1) {
                         // We are inside of a wall
-                        in_wall = true;
-
-                        auto is_solid = [&](Vec2 pos) {
-                            int col = layer.translate_world_x_col(pos.x);
-                            int row = layer.translate_world_y_row(pos.y);
-                            Tile tile = layer.get_tile(col, row);
-
-                            if (tile.atlas_idx < 0 || tile.tile_idx < 0) return false;
-
-                            return scene.loaded_atlases[tile.atlas_idx]
-                                .tile_data[tile.tile_idx]
-                                .collision_data == CollisionType::COLL_FULL_SOLID;
-                        };
-
-                        // Check center of body
-                        Vec2 center = transform.position;
-
-                        // If inside wall → push out horizontally
-                        if (is_solid(center)) {
-                            float push_dist = 0.0f;
-                            const float MAX_PUSH = body.size.x;
-
-                            // Try pushing right first
-                            while (push_dist < MAX_PUSH) {
-                                center.x += 1.0f;
-                                if (!is_solid(center)) {
-                                    transform.position.x = center.x;
-                                    break;
-                                }
-                                push_dist += 1.0f;
-                            }
-
-                            // If still stuck, try left
-                            if (push_dist >= MAX_PUSH) {
-                                center = transform.position;
-                                push_dist = 0.0f;
-
-                                while (push_dist < MAX_PUSH) {
-                                    center.x -= 1.0f;
-                                    if (!is_solid(center)) {
-                                        transform.position.x = center.x;
-                                        break;
-                                    }
-                                    push_dist += 1.0f;
-                                }
-                            }
-                        }
-
-
-
+                        to_move.x = 0.0f;
                         break;
 
                     } else {
@@ -118,10 +77,58 @@ void PhysicsBodyMovementSystem::update (Registry & registry, float deltatime) {
                     velocity.magnitude.x = 0;
                 }
             }
+        
 
-            
+
+            // VERTICAL COLLISION
+
+            // Cast vertical rays
+
+            if (direction_y > 0) {
+                for (int i = 0; i < VERT_RAY_COUNT; i++) {
+                    float ray_x = (transform.position.x - body.size.x / 2.0f) + ((body.size.x * i) / (float)(VERT_RAY_COUNT - 1));
+                    vert_ray_dists[i] = layer.raycast(scene, {ray_x, transform.position.x + body.size.x / 2.0f}, {0.0f, 1.0f}, (int)std::ceil(std::fabs(to_move.y)));
+                }
+                
+            } else if (direction_x < 0) {
+                for (int i = 0; i < VERT_RAY_COUNT; i++) {
+                    float ray_x = (transform.position.x - body.size.y / 2.0f) + ((body.size.x * i) / (float)(VERT_RAY_COUNT - 1));
+                    vert_ray_dists[i] = layer.raycast(scene, {ray_x, transform.position.x - body.size.x / 2.0f}, {0.0f, -1.0f}, (int)std::ceil(std::fabs(to_move.y)));
+                }
+
+            } else {
+                break;
+            }
+
+            // Set move to vertical ray calc dist min clamp to collision
+
+            for (float dist : vert_ray_dists) {
+                if (dist < std::fabs(to_move.y)) {
+                    // We cannot move full desired distance for some reason
+
+                    if (dist == -1) {
+                        // We are inside of a wall
+                        to_move.y = 0.0f;
+                        break;
+
+                    } else {
+                        // We are not inside of a wall, we just can't move our full desired distance
+
+                        int move_axis = (to_move.y > 0) ? 1 : -1;
+                        to_move.y = (dist - body.skin) * move_axis;
+                    }
+                    
+                    velocity.magnitude.y = 0;
+                }
+            }
 
 
+        
+        
+        
+        
+        
+        
         }
         
 
